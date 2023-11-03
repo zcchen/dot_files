da1dbb2d94fad788994a688d36d0bb3d6d9095d3
zcchen <Bill.Chen.fs.cn@outlook.com>
2023-11-04 00:17:33 +0800

    Added google driver downloader script

---
+++ bash/google-download.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+set -x
+
+CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+wget -c --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+rm -rf /tmp/cookies.txt
